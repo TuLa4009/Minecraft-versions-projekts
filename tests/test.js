37dee92b26f444a3b75fb57cbca9857fa31f9d15
TuLa4009 <loqimusic4009@gmail.com>
2026-04-20 17:11:44 +0300

    Update quiz questions and answers in test.js

--- tests/test.js
+++ tests/test.js
@@ -1,83 +1,88 @@
 const jautajumi =  [
     {
-        text: "Jaut1",
-        answers: ["1", "2", "3", "4"],
+        text: "q1.question",
+        answers: ["q1.a", "q1.b", "q1.c", "q1.d"],
+        correct: 2
+    },
+    {
+        text: "q2.question",
+        answers: ["q2.a", "q2.b", "q2.c", "q2.d"],
         correct: 0
     },
     {
-        text: "Jaut2",
-        answers: ["1", "2", "3", "4"],
+        text: "q3.question",
+        answers: ["q3.a", "q3.b", "q3.c", "q3.d"],
+        correct: 2
+    },
+    {
+        text: "q4.question",
+        answers: ["q4.a", "q4.b", "q4.c", "q4.d"],
+        correct: 3
+    },
+    {
+        text: "q5.question",
+        answers: ["q5.a", "q5.b", "q5.c", "q5.d"],
+        correct: 1
+    },
+    {
+        text: "q6.question",
+        answers: ["q6.a", "q6.b", "q6.c", "q6.d"],
+        correct: 3
+    },
+    {
+        text: "q7.question",
+        answers: ["q7.a", "q7.b", "q7.c", "q7.d"],
+        correct: 1
+    },
+    {
+        text: "q8.question",
+        answers: ["q8.a", "q8.b", "q8.c", "q8.d"],
+        correct: 1
+    },
+    {
+        text: "q9.question",
+        answers: ["q9.a", "q9.b", "q9.c", "q9.d"],
+        correct: 2
+    },
+    {
+        text: "q10.question",
+        answers: ["q10.a", "q10.b", "q10.c", "q10.d"],
+        correct: 3
+    },
+    {
+        text: "q11.question",
+        answers: ["q11.a", "q11.b", "q11.c", "q11.d"],
+        correct: 3
+    },
+    {
+        text: "q12.question",
+        answers: ["q12.a", "q12.b", "q12.c", "q12.d"],
+        correct: 3
+    },
+    {
+        text: "q13.question",
+        answers: ["q13.a", "q13.b", "q13.c", "q13.d"],
+        correct: 2
+    },
+    {
+        text: "q14.question",
+        answers: ["q14.a", "q14.b", "q14.c", "q14.d"],
         correct: 0
     },
     {
-        text: "Jaut3",
-        answers: ["1", "2", "3", "4"],
-        correct: 0
+        text: "q15.question",
+        answers: ["q15.a", "q15.b", "q15.c", "q15.d"],
+        correct: 3
     },
     {
-        text: "Jaut4",
-        answers: ["1", "2", "3", "4"],
-        correct: 0
+        text: "q16.question",
+        answers: ["q16.a", "q16.b", "q16.c", "q16.d"],
+        correct: 2
     },
     {
-        text: "Jaut5",
-        answers: ["1", "2", "3", "4"],
-        correct: 0
-    },
-    {
-        text: "Jaut6",
-        answers: ["1", "2", "3", "4"],
-        correct: 0
-    },
-    {
-        text: "Jaut7",
-        answers: ["1", "2", "3", "4"],
-        correct: 0
-    },
-    {
-        text: "Jaut8",
-        answers: ["1", "2", "3", "4"],
-        correct: 0
-    },
-    {
-        text: "Jaut9",
-        answers: ["1", "2", "3", "4"],
-        correct: 0
-    },
-    {
-        text: "Jaut10",
-        answers: ["1", "2", "3", "4"],
-        correct: 0
-    },
-    {
-        text: "Jaut11",
-        answers: ["1", "2", "3", "4"],
-        correct: 0
-    },
-    {
-        text: "Jaut12",
-        answers: ["1", "2", "3", "4"],
-        correct: 0
-    },
-    {
-        text: "Jaut13",
-        answers: ["1", "2", "3", "4"],
-        correct: 0
-    },
-    {
-        text: "Jaut14",
-        answers: ["1", "2", "3", "4"],
-        correct: 0
-    },
-    {
-        text: "Jaut15",
-        answers: ["1", "2", "3", "4"],
-        correct: 0
-    },
-    {
-        text: "Jaut16",
-        answers: ["1", "2", "3", "4"],
-        correct: 0
+        text: "q17.question",
+        answers: ["q17.a", "q17.b", "q17.c", "q17.d"],
+        correct: 3
     },
 ]
 
